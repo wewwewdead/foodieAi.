54a37814e12d063750d8c35e71b8dc6f551a7ca6
john mathew <johnmathewloren27@gmail.com>
2025-04-26 16:03:04 +0900

    fix

--- server/routes/gemini.js
+++ server/routes/gemini.js
@@ -1,10 +1,10 @@
-import { GoogleGenAI, Type } from "@google/genai";
+import { GoogleGenAI } from "@google/genai";
 import multer from "multer";
 import express from "express";
 import 'dotenv/config';
-import { config } from "dotenv";
 
-// Initialize Google Generative AI with API Key
+
+// initialize google ai with api Key
 const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
 
 const router = express.Router();
