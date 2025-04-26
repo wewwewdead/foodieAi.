12f219657de82c4bac794386eb9395895bb28cce
john mathew <johnmathewloren27@gmail.com>
2025-04-26 14:09:01 +0900

    finalize

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(cors({
-    origin:['http://localhost:5173'],
+    origin:['https://foodieai-1-g7lh.onrender.com','http://localhost:5173'],
     methods: "GET,POST,PUT,DELETE",
     credentials: true
 }));
