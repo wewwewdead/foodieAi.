9631b0cb82b5d7c1279aea3bc9bcca29a00f453c
wewwewdead <johnmathewloren27@gmail.com>
2025-05-25 14:58:50 +0900

    Revert "Fix import issues"

--- client/src/component/Footer.jsx
+++
@@ -1,8 +0,0 @@
-const Footer = () => {
-  return (
-    <footer style={{ color: "#0f0f0f", margin: 0, padding: 0 }}>
-      <p>&copy; 2025 Foodie @loren. All rights reserved.</p>
-    </footer>
-  );
-};
-export default Footer;
