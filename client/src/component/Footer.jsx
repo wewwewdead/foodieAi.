6ef0717d9a227b1b0c11fff4bb0e4259ab647871
wewwewdead <johnmathewloren27@gmail.com>
2025-05-26 06:56:51 +0900

    Revert "style: improve home content alignment"

--- client/src/component/Footer.jsx
+++ client/src/component/Footer.jsx
@@ -1,9 +1,8 @@
+import { footer } from "framer-motion/client";
+
 const Footer = () => {
   return (
-    <footer
-      style={{ color: "#0f0f0f", margin: 0, padding: 0 }}
-      className="footer"
-    >
+    <footer style={{ color: "#0f0f0f", margin: 0, padding: 0 }}>
       <p>&copy; 2025 Foodie @loren. All rights reserved.</p>
     </footer>
   );
